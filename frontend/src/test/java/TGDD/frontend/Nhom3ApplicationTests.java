0696d28512319b1c68f7d56fb765c597a85097d6
AnNgLeHoang <hoanganngle2001@gmail.com>
2022-06-23 21:02:37 +0700

    add new project

---
+++ frontend/src/test/java/TGDD/frontend/Nhom3ApplicationTests.java
@@ -0,0 +1,13 @@
+package TGDD.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
